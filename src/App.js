082e8dc86afc21e73833431f002059fcb2b8606f
sashida <sashida@primagest.co.jp>
2020-09-14 08:41:33 +0900

    p.254-3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-export default () => 
+export const App = () => 
   <h1 className="bg-primary text-white text-center p-2">
     Hello Adam
   </h1>
+
+export default App;
+
