54c2bfe0cc7a492b8519beab07adf634bed220a7
sashida <sashida@primagest.co.jp>
2020-09-14 12:48:20 +0900

    p.262

--- src/Message.js
+++ src/Message.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 export function Message(props) {
-    return <h4 className="bg-success text-white text-center p-2">
-        { props.greeting }, { props.name }
-    </h4>
+    if (props.name === "Bob") {
+        return (
+            <h4 className="bg-warning p-2">
+                { props.greeting }, { props.name }
+            </h4>
+        );
+    } else {
+        return (
+            <h4 className="bg-success text-white text-center p-2">
+                { props.greeting }, { props.name }
+            </h4>        
+        )
+    }
+    return 
 }
