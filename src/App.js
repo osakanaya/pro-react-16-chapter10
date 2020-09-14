9f411d2c166292b9d79ee5244296384fedc3e5a5
sashida <sashida@primagest.co.jp>
2020-09-14 13:01:22 +0900

    p.270-1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { Summary } from "./Summary";
 
+let names = ["Bob", "Alice", "Dora"];
+
 export default function App() { 
   return (
-    <div>
-      <h1 className="bg-primary text-white text-center p-2">
-        Hello Adam
-      </h1>
-      <Summary names={ ["Bob", "Alice", "Dora"] }/>
-    </div>
-  )
+    <table className="table table-sm table-striped">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Letters</th>
+        </tr>
+      </thead>
+      <tbody>
+        { names.map((name, index) => 
+          <tr key={ name }>
+            <Summary index={ index } name={ name } />
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
 }
 
