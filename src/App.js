760a02ae8cb4fa946b8fe9a2735b66d794bd4303
sashida <sashida@primagest.co.jp>
2020-09-14 12:46:27 +0900

    p.261

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ export default function App() {
       <h1 className="bg-primary text-white text-center p-2">
         Hello Adam
       </h1>
-      <Message />
-      <Message />
-      <Message />
+      <Message greeting="Hello" name="Bob" />
+      <Message greeting="Hola" name={ "Alice" + "Smith" } />
+      <Message greeting="Hi there" name="Dora" />
       <Summary />
     </div>
   )
