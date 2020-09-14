3f925e38462ff8b4cfbc75b69201ee67264d8adc
sashida <sashida@primagest.co.jp>
2020-09-15 01:13:06 +0900

    p.271"

--- src/Summary.js
+++ src/Summary.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 export function Summary(props) {
-    return <>
+    if (props.name.length >= 4) {
+        return <React.Fragment>
             <td>{ props.index + 1 }</td>
             <td>{ props.name }</td>
             <td>{ props.name.length }</td>
-        </>
+        </React.Fragment>
+    }else {
+        return null;
+    }
 }
