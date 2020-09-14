f25501252be6aedf740a7ae32ec869025f573f67
sashida <sashida@primagest.co.jp>
2020-09-14 12:58:00 +0900

    p.268

--- src/Summary.js
+++ src/Summary.js
@@ -1,17 +1,9 @@
 import React from "react";
 
-function createInnerElements(names) {
-    return names.map(name => 
-        <div>
-            { `${name} contains ${name.length} letters`}
-        </div>
-    );
-}
-
 export function Summary(props) {
     return <h4 className="bg-info text-white text-center p-2">
         { props.names.map(name => 
-                <div>
+                <div key={ name }>
                     { `${name} contains ${name.length} letters`}
                 </div>
             )
