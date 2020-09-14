0f74ea742bf49a11f9717e636d8ad93c3110f32c
sashida <sashida@primagest.co.jp>
2020-09-15 01:22:56 +0900

    p.280

---
+++ src/CallbackButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function CallbackButton(props) {
+    return (
+        <button className={ `btn btn-${props.theme} btn-sm m-1`} onClick={ props.callback }>
+            { props.text }
+        </button>
+    );
+}
