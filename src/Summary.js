e9265dee1d27f2adc6210f4ff63d32ba32f2a0b5
sashida <sashida@primagest.co.jp>
2020-09-14 13:01:42 +0900

    p.270-2

--- src/Summary.js
+++ src/Summary.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 export function Summary(props) {
-    return <React.Fragment>
+    return <>
             <td>{ props.index + 1 }</td>
             <td>{ props.name }</td>
             <td>{ props.name.length }</td>
-        </React.Fragment>
+        </>
 }
