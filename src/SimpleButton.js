a7846faee00279c127712875595a05d358674608
sashida <sashida@primagest.co.jp>
2020-09-16 09:14:40 +0900

    p.295

--- src/SimpleButton.js
+++ src/SimpleButton.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 
 export class SimpleButton extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            counter: 0,
+            hasButtonBeenClicked: false
+        };
+    }
+
     render() {
         return (
             <button onClick={ this.props.callback } className={ this.props.className }
             disabled={ this.props.disabled === "true" || this.props.diabled === true }>
-            { this.props.text }
+            { this.props.text } { this.state.counter }
+            { this.state.hasButtonBeenClicked && <div>Button Clicked!</div> }
         </button>
         );
     }
